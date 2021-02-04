d8a39ddc430a8843955d7115a5c1e00d0d25481c
AllyxMiko <14049550@qq.com>
2021-02-05 00:56:07 +0800

    更新数据库配置项

---
+++ config/database.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @File    :   database.py
+# @Time    :   2021/02/05 00:27:28
+# @Author  :   Allyx
+# @Email   :   allyxmiko@163.com
+# @Version :   1.0
+
+# Here put the import lib
+
+'''
+    数据库配置参数说明:
+        host: 数据库地址，默认127.0.0.1
+        user: 数据库用户名
+        password: 数据库密码
+        database: 数据库名称
+        charset: 数据库编码，默认utf8
+'''
+DatabaseConfig = {
+    "host": "127.0.0.1",
+    "user": "",
+    "password": "",
+    "database": "",
+    "charset": "utf8"
+}
