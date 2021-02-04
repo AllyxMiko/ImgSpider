d3fc3d13b8a79102211e1b6ff3f138b75acade0d
AllyxMiko <14049550@qq.com>
2021-02-05 00:43:59 +0800

    添加程序入口

---
+++ ImgSpider.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @File    :   ImgSpider.py
+# @Time    :   2021/02/05 00:42:54
+# @Author  :   Allyx
+# @Email   :   allyxmiko@163.com
+# @Version :   1.0
+
+# Here put the import lib
